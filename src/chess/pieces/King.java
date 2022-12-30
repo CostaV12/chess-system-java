ce99fff7d827ee1f1b1fe67446d59bc3068a30a1
Vini Costa <vc896536@gmail.com>
2022-12-30 13:46:12 -0300

    Implementing possible moves of King

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -15,9 +16,56 @@ public class King extends ChessPiece {
         return "K";
     }
 
+    private boolean canMove(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p == null || p.getColor() != getColor();
+    }
+
+    private boolean setTrueInMatPossibleMoves(boolean[][] mat, Position position) {
+        if (getBoard().positionExists(position) && canMove(position)) {
+            mat[position.getRow()][position.getColumn()] = true;
+        }
+        return false;
+    }
+
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0,0);
+
+        //above
+        p.setValues(position.getRow() - 1, position.getColumn());
+        setTrueInMatPossibleMoves(mat, p);
+
+        //above
+        p.setValues(position.getRow() + 1, position.getColumn());
+        setTrueInMatPossibleMoves(mat, p);
+
+        //left
+        p.setValues(position.getRow(), position.getColumn() - 1);
+        setTrueInMatPossibleMoves(mat, p);
+
+        //right
+        p.setValues(position.getRow(), position.getColumn() + 1);
+        setTrueInMatPossibleMoves(mat, p);
+
+        //nw
+        p.setValues(position.getRow() - 1, position.getColumn() - 1);
+        setTrueInMatPossibleMoves(mat, p);
+
+        //ne
+        p.setValues(position.getRow() - 1, position.getColumn() + 1);
+        setTrueInMatPossibleMoves(mat, p);
+
+        //sw
+        p.setValues(position.getRow() + 1, position.getColumn() - 1);
+        setTrueInMatPossibleMoves(mat, p);
+
+        //se
+        p.setValues(position.getRow() + 1, position.getColumn() + 1);
+        setTrueInMatPossibleMoves(mat, p);
+
         return mat;
     }
 }
